b650528fd270c4b6c094b54f21efbbd22c8891ee
N-Z-K <119885111+N-Z-K@users.noreply.github.com>
2022-12-07 21:00:30 +0400

    Add files via upload

---
+++ NSI projet/Flask.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 25 13:09:05 2022
+
+@author: Noe Gebert/Onill Pydannah/SHAYAN sHAKUN
+"""
+import sqlite3
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/enregdestination')
+def enregdestination():
+
+    result = request.form 
+    nom = ['nom']
+    continent = ['continent']
+    temps_moyen = ['temps_moyen']
+    note_des_voyageurs = ['note_des_voyageurs']
+
+
+    if nom != '' and continent != '' and temps_moyen != '' and note_des_voyageurs != '':
+        
+        conn = sqlite3.connect('../bdd/BDD_Agence.db') # connection à la BDD
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO destinations (nom,continent,temps_moyenne,note_voyageurs VALUES (?,?,?,?))"(nom,continent,temps_moyen,note_des_voyageurs))
+
+        return render_template("enregdestination.html")
+
+
+    else :
+        #revenir à la page réservation
+        return render_template('enregdestination.html', message='formulaire non valide')
+
+
+
+    
+
+
+@app.route('/enregvol')
+def enregvol():
+    return render_template("enregvol.html")
+
+
+@app.route('/enregclient')
+def enregclient():
+    return render_template("enregclient.html")
+
+
+@app.route('/enreglogement')
+def enreglogement():
+    return render_template("enreglogement.html")
+
+
+@app.route('/verifdispovol')
+def verifdispovol():
+    return render_template("verifdispovol.html")
+
+
+@app.route('/verifdispologement')
+def verifdispologement():
+    return render_template("verifdispologement.html")
+
+
+@app.route('/reservvol')
+def reservvol():
+    return render_template("reservvol.html")
+
+
+@app.route('/reservlogement')
+def reservlogement():
+    return render_template("reservlogement.html")
+
+
+app.run(debug=True)
+
+
---
+++ NSI projet/static/custom.js
@@ -0,0 +1,30 @@
+
+function btnnvdestination() 
+    {window.location.href = "../templates/enregdestination.html";}
+
+function enregvol() 
+    {window.location.href = "../templates/enregvol.html";}
+
+function enregclient() 
+    {window.location.href = "../templates/nregclient.html";}
+
+function enreglogement() 
+    {window.location.href = "../templates/enreglogement.html";}
+
+function verifdispovol() 
+    {window.location.href = "../templates/verifdispovol.html";}
+
+function verifdispologment() 
+    {window.location.href = "../templates/verifdispologment.html";}
+
+function reservvol() 
+    {window.location.href = "../templates/reservvol.html";}
+
+function reservlogement() 
+    {window.location.href = "../templates/reservlogement.html";}
+
+function buttonover() 
+    {
+        x=getElementById()
+        X.style.background;
+    }
